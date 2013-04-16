879f2cc0d14d919349d2e20f33be305b40f41e7f
Wil Asche <wil@asche.us>
2013-04-16 10:41:51 -0400

    add style, render in sandbox

--- src/app.js
+++ src/app.js
@@ -1,24 +1,43 @@
-function createOverlay( form ) {
-  var options = {
-    content : form.content.value
-  , showCloseButton : form.close.checked
-  , autoShow : form.autoShow.checked
-  };
 
-  for (var i = form.backdrop.length - 1; i >= 0; i--) {
-    if ( form.backdrop[ i ].checked ) {
-      options.backdrop = parseInt( form.backdrop[ i ].value );
-      break;
+var app = (function (){
+
+  var overlay
+    , app = {};
+
+  app.createOverlay = function( form ) {
+    var options = {
+      content : form.content.value
+    , showCloseButton : form.close.checked
+    , autoShow : form.autoShow.checked
+    , closeContent : '&times;'
+    };
+
+    for (var i = form.backdrop.length - 1; i >= 0; i--) {
+      if ( form.backdrop[ i ].checked ) {
+        options.backdrop = parseInt( form.backdrop[ i ].value );
+        break;
+      }
     }
-  };
 
-  for (i = form.position.length - 1; i >= 0; i--) {
-    if ( form.position[ i ].checked ) {
-      options.position = Overlay[ 'Position' + form.position[ i ].value ];
+    for (i = form.position.length - 1; i >= 0; i--) {
+      if ( form.position[ i ].checked ) {
+        options.position = Overlay[ 'Position' + form.position[ i ].value ];
+      }
     }
-  };
 
-  console.log( options );
+    for (i = form.style.length - 1; i >= 0; i--) {
+      if ( form.style[ i ].checked ) {
+        options.style = form.style[ i ].value;
+      }
+    }
 
-  return new Overlay( options );
-}
+console.log( options );
+    if ( overlay ) { overlay.destroy(); }
+    overlay = new Overlay( options );
+
+    return overlay;
+  }
+
+  return app;
+
+}());
